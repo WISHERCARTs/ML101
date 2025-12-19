ee64eef54c68c07018f47891fb08321a4525cf9e
Wish Nakthong <wishza2549@gmail.com>
2025-12-19 13:50:30 +0700

    แสดงภาพตัวเลข MNIST

--- ML2.py
+++ ML2.py
@@ -1,21 +1,20 @@
-from scipy.io import loadmat
+from scipy.io import loadmat # Load the MNIST dataset
 import matplotlib.pyplot as plt
 
 mnist_raw = loadmat("mnist-original.mat")
-
 # print(mnist_raw)
 mnist = {
     "data": mnist_raw["data"].T,
     "target": mnist_raw["label"][0]
 }
 x,y = mnist["data"],mnist["target"]
-number = x[15000]
+
+number = x[2000]
 number_image = number.reshape(28,28) # Reshape the number to a 28x28 image
 
+print(y[2000])
 plt.imshow(
     number_image,
     cmap=plt.cm.binary,
     interpolation="nearest")
 plt.show()
-
-print(x.shape)
